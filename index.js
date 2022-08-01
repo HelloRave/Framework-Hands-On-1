984d5072bf5fe56cc7da1a4b2410cb4b15a21b4e
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-08-01 04:00:55 +0000

    Added csrf

--- index.js
+++ index.js
@@ -4,26 +4,10 @@ const wax = require('wax-on')
 const session = require('express-session')
 const flash = require('connect-flash')
 const FileStore = require('session-file-store')(session)
+const csrf = require('csurf')
 require('dotenv').config()
 
 const app = express()
-
-app.use(session({
-    store: new FileStore(),
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}))
-
-app.use(flash())
-
-app.use(function(req, res, next){
-    res.locals.success_messages = req.flash('success_messages')
-    res.locals.error_messages = req.flash('error_messages')
-    next()
-})
-
-const posterRoutes = require('./routes/poster')
 
 app.set('view engine', 'hbs')
 
@@ -35,6 +19,31 @@ wax.setLayoutPath('./views/layouts')
 app.use(express.urlencoded({
     extended: false 
 }))
+
+app.use(session({
+    store: new FileStore(),
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(csrf())
+
+app.use(function(req, res, next){
+    res.locals.csrfToken = req.csrfToken()
+    next()
+})
+
+app.use(flash())
+
+app.use(function(req, res, next){
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    res.locals.user = req.session.user
+    next()
+})
+
+const posterRoutes = require('./routes/poster')
 
 app.use('/', require('./routes/landing'))
 app.use('/posters', posterRoutes)
